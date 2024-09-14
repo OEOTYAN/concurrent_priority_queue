af3601fa93990f12712ceaa012b57a3f68f27306
OEOTYAN <OEOTYAN@outlook.com>
2024-09-15 01:30:27 +0800

    init v0.1.0

---
+++ xmake.lua
@@ -0,0 +1,9 @@
+add_repositories("oeo-repo https://github.com/OEOTYAN/xmake-repo.git")
+
+add_requires("outility v0.1.0")
+
+target("concurrent_priority_queue")
+    set_kind("headeronly")
+    add_headerfiles("include/(**.h)")
+    set_languages("c++20")
+    add_packages("outility")
